5d8a07c1acdb69689d400fbd1fce860e2aa551a4
selfcanonical <selfcanonical@gmail.com>
2024-07-04 00:53:09 +0600

    Create squarespace-dashboard.js

---
+++ squarespace-dashboard.js
@@ -0,0 +1,1 @@
+javascript:(function(){var originalUrl=window.location.href;var host=window.location.host;var newUrl=window.location.protocol + '//' + host + '/config/';window.location.href=newUrl;})();
